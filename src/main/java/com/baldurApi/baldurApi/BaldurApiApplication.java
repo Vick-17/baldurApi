b2454aafe3169fa738c37bbc2770eaa7969e5fe1
Vega Victor <vega.victor.0712@gmail.com>
2023-09-07 11:58:41 +0200

    initial commit

---
+++ src/main/java/com/baldurApi/baldurApi/BaldurApiApplication.java
@@ -0,0 +1,13 @@
+package com.baldurApi.baldurApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaldurApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaldurApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/baldurApi/baldurApi/BaldurApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baldurApi.baldurApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaldurApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
